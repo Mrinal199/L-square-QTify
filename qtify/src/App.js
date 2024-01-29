f295aa23e7a7b881d42a42fec266fd3586070dab
Mrinal199 <mrinalsuman11@gmail.com>
2024-01-29 23:23:37 +0530

    Minor Fix

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,11 +1,16 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
-    <><Navbar/></>
+    <><Navbar/>
+    
+    <h1>Hi</h1>
+    </>
+
   );
 }
 
