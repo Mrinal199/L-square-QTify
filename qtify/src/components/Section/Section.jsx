6130bb50543d3869a4c7f891fca3647f9439d6a8
Mrinal199 <mrinalsuman11@gmail.com>
2024-02-01 04:13:52 +0530

    RRR777Ak47

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,36 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./Section.module.css";
 import {  CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
 import Carousel from "../Carousel/Carousel";
+import Filters from "../Filters/Filters";
 
 
-export default function Section({ title, data, type}) {
+export default function Section({ title, data, filterSource, type}) {
     const [carouselToggle, setCarouselToggle] = useState(true);
+    const [filters, setFilters] = useState([{key: "all", label: "All"}]);
+    const [selectedFilterIndex, setSelectedFilterIndex] = useState(0);
     const handleToggle = () => {
         setCarouselToggle((prevState) => !prevState);
     };
+    useEffect(() => {
+            if (filterSource) {
+                filterSource().then((response) => {
+                    const { data } = response;
+                    setFilters([...filters, ...data]);
+                })
+            }
+    }, [ ]);
+    const showFilters = filters.length > 1;
+    const cardToRender = data.filter((card) => showFilters && selectedFilterIndex !== 0 ? card.genre.key === filters[selectedFilterIndex].key : card );
     return (
         <div>
             <div className={styles.header}>
                 <h3>{title}</h3>
-                <h4 className={styles.toggleText} onClick={handleToggle}>
+                {!showFilters && (<h4 className={styles.toggleText} onClick={handleToggle}>
                     {!carouselToggle ? "Collapse All": 
-                "Show All"}</h4>
+                "Show All"}</h4>)}
             </div>
-            {data.length === 0 ? (
+            {showFilters && (
+                <div className={styles.filterWrapper}>
+                    <div className={styles.header}>
+                            <Filters
+                                filters={filters}
+                                selectedFilterIndex={selectedFilterIndex}
+                                setSelectedFilterIndex={setSelectedFilterIndex}
+                            />
+                    </div>
+                </div>
+            )}
+            {cardToRender.length === 0 ? (
                 <CircularProgress />
             ) : (
                         <div className={styles.cardWrapper}>
                             {!carouselToggle ? (
                                 <div className={styles.wrapper}>
-                                {data.map((ele, index) => (
+                                {cardToRender.map((ele, index) => (
                                     <Card key={index} data={ele} type={type} />
                                 ))}
                                </div>
                     ) : ( 
                        <Carousel
-                                data={data}
+                                data={cardToRender}
                                 renderComponent={(data) => <Card key={data.id} data={data} type={type} />}
                                 />
                     )}
